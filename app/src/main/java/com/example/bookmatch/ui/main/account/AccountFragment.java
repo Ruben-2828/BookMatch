12b09c1ca4360360287d8cdc7e509e2f01dacc06
Ruben <rubentenderini@gmail.com>
2024-03-14 10:30:18 +0100

    Feat: account statistics and account option menu

--- app/src/main/java/com/example/bookmatch/ui/main/account/AccountFragment.java
+++ app/src/main/java/com/example/bookmatch/ui/main/account/AccountFragment.java
@@ -1,16 +1,25 @@
 package com.example.bookmatch.ui.main.account;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.bookmatch.R;
 import com.example.bookmatch.databinding.FragmentAccountBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AccountFragment extends Fragment {
 
@@ -22,6 +31,42 @@ public class AccountFragment extends Fragment {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.editAccountButton.setOnClickListener(v -> {
+            PopupMenu popup = new PopupMenu(getContext(), v);
+            popup.getMenuInflater().inflate(R.menu.account_option_menu, popup.getMenu());
+
+            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+
+                    int id = item.getItemId();
+                    if (id == R.id.edit_profile_item) {
+                        //TODO: implement edit profile
+                        Snackbar.make(view, "Edit profile", Snackbar.LENGTH_LONG).show();
+                        return true;
+                    }
+                    if (id == R.id.about_us_item) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Ruben-2828/BookMatch/tree/main"));
+                        startActivity(browserIntent);
+                        return true;
+                    }
+                    if (id == R.id.logout_item) {
+                        //TODO: implement logout
+                        Snackbar.make(view, "Logout", Snackbar.LENGTH_LONG).show();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+            popup.show();
+        });
     }
 
     @Override
